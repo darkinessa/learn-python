d059a82fac1fb1562b1ee5aad368e3d0ab2cb6e2
Irina Koroleva <korolevai@gmail.com>
2019-02-28 21:16:16 +0300

    Вынесла токен в отдельный локальный файл, добавила этот файл в гит игнор

--- bot_.py
+++ bot_.py
@@ -1,19 +1,24 @@
+import settings_bot
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+         'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
 
 def greet_user(bot, update):
-	print('Вызван /start')
-	print(update)
+    print('Вызван /start')
+    print(update)
+
 
 def talk_to_me(bot, update):
-    user_text = update.message.text 
+    user_text = update.message.text
     print(user_text)
-    update.message.reply_text(user_text) 	
+    update.message.reply_text(user_text)
+
 
 def main():
-    mybot = Updater("779768962:AAEWoorfNmB30ZEtCqFqFZkmwNBqoC48KwM", request_kwargs=PROXY)
+    mybot = Updater(settings_bot.SECRET_BOT_KEY, request_kwargs=PROXY)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
